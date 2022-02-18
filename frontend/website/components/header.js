6516cfd35935914703e7a946fa83be00131fc458
Edwin Hernandez <Edwinhr716@utexas.edu>
2022-02-17 19:16:31 -0600

    Created Next.js project, a file for each page that we plan to have, and crated a header component

---
+++ frontend/website/components/header.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+
+const Header = () => {
+    return(
+        <AppBar
+        position="relative"
+        elevation={3}
+        color = "secondary"
+      >
+        <Toolbar sx={{ flexWrap: 'wrap' }}>
+          <Typography variant="h6" color="white" noWrap sx={{ flexGrow: 1 }}>
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="#FFFFFF"
+              href="/resources"
+              //my is margin top & bottom, mx is margin left & right
+              sx={{ my: 1, mx: 1.5}}
+              style = {{textDecoration:'none'}}
+            >
+              resources
+            </Link>
+            <Link
+              variant="button"
+              color="#ffffff"
+              href="/projects"
+              sx={{ my: 1, mx: 1.5 }}
+              style = {{textDecoration:'none'}}
+            >
+              projects
+            </Link>
+            <Link
+              variant="button"
+              color="#ffffff"
+              href="/datasets"
+              sx={{ my: 1, mx: 1.5 }}
+              style = {{textDecoration:'none'}}
+            >
+              Datasets
+            </Link>
+          </nav>
+          <Button href="#" variant="outlined" sx={{ my: 1, mx: 1.5 }}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    )
+}
+
+export default Header
