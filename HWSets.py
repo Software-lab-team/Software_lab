f0836469d77ecf8444f3f0b15c1e660f8ca9b40e
Mark Liao <liaozhoudi@gmail.com>
2022-03-23 17:59:20 -0500

    Finished first working version of HWSets

--- HWSets.py
+++ HWSets.py
@@ -1,3 +1,4 @@
+from os import abort
 from flask import Flask, jsonify, request, Blueprint
 from pymongo import MongoClient
 
@@ -9,32 +10,34 @@ db = client.Main
 hwCol = db.HWSets
 
 # Properties
-# Hardware Set Name
-# Capacity
-# Availability
+# name
+# capacity
+# availability
 
-#/HWSets?var1=mark&var2=liao
-@HWSets.route("/HWSets")
-def home():
-    var1 = request.args.get('var1')
-    var2 = request.args.get('var2')
-    return "this calls a func in hwsets " + var1 + var2
+#/HWSets
+@HWSets.route('/HWSets', methods=['GET'])
+def getAllHWSets():
+    output = []
+    for hw in hwCol.find():
+        output.append({'name': hw['name'], 'capacity': hw['capacity'], 'availability': hw['availability']})
+    return {'result': output}
+    # return jsonify({'result': output})
 
 #/HWSets?name=HWSet1&capacity=100
 @HWSets.route('/HWSets', methods=['POST'])
 def addHWSet():
     name = request.args.get('name', type=str)
     capacity = request.args.get('capacity', type=int)
-    # if hwCol.count_documents({'name': name}, limit=1) != 0:
-    #     abort()
+    if hwCol.count_documents({'name': name}, limit=1) != 0:
+        return 'Error: HWSet already exists', 400
     article = {
-        "Name": name,
-        "Capacity": capacity,
-        "Availability": capacity
+        "name": name,
+        "capacity": capacity,
+        "availability": capacity
     }
 
     hwCol.insert_one(article)
-    articleAdded = hwCol.find_one({'Name': name})
+    articleAdded = hwCol.find_one({'name': name})
     print(articleAdded)
     del articleAdded['_id']
 
