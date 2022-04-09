0d564272c2bf2b3ccc11abd68d7c293c37ef0069
Mark Liao <liaozhoudi@gmail.com>
2022-04-09 18:39:27 -0500

    can get metadata for circor and taichi

--- Datasets.py
+++ Datasets.py
@@ -1,10 +1,36 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request
 import os
 import wfdb
+import shutil
 
 app = Flask(__name__)
 Datasets = Blueprint("Datasets", __name__)
 
+def get_circor():
+    file_list = ['training_data/13918_AV.hea']
+    cwd = os.getcwd()
+    dl_dir = os.path.join(cwd, 'tmp_dl_dir')
+    wfdb.dl_files('circor-heart-sound', dl_dir, file_list)
+    record = wfdb.rdheader('tmp_dl_dir/training_data/13918_AV') 
+    shutil.rmtree(dl_dir)
+    return str(record.__dict__)
+
+def get_taichi():
+    file_list = ['Dual-task/S0088_DT_V1.hea']
+    cwd = os.getcwd()
+    dl_dir = os.path.join(cwd, 'tmp_dl_dir')
+    wfdb.dl_files('taichidb', dl_dir, file_list)
+    record = wfdb.rdheader('tmp_dl_dir/Dual-task/S0088_DT_V1')
+    shutil.rmtree(dl_dir) 
+    return str(record.__dict__)
+
+#/Datasets?dataset=circor
 @Datasets.route("/Datasets")
 def test():
-    return "this calls a func in Datasets"
+    dataset = request.args.get('dataset', type=str)
+    if(dataset == 'circor'):
+        return get_circor()
+    elif(dataset == 'taichi'):
+        return get_taichi()
+    else:
+        return 'Dataset not found', 404
